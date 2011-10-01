a499a0d246dec8afc5b3b10219de02e115c89e03
Frank Wang <frankyanwang@gmail.com>
2011-10-01 02:01:10 -0700

    fixed a find and place error. should be "post" instead of "div"

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -6,7 +6,7 @@ class ProposalsController < ApplicationController
   end
   
   def my_proposals
-    my_proposals = current_user.proposals.includes(:div, :trade_div)
+    my_proposals = current_user.proposals.includes(:post, :trade_post)
     respond_to do |format|
       # format.html { render "myproposals"}
       format.html { render :partial => "my_proposals", :locals => { :my_proposals => my_proposals }}
@@ -15,7 +15,7 @@ class ProposalsController < ApplicationController
   end
   
   def proposals_to_me
-    proposals_to_me = Proposal.where(:div_id => current_user.divs)
+    proposals_to_me = Proposal.where(:post_id => current_user.posts)
     respond_to do |format|
       # format.html { render "myproposals"}
       format.html { render :partial => "proposals_to_me", :locals => { :proposals_to_me => proposals_to_me }}
@@ -24,35 +24,35 @@ class ProposalsController < ApplicationController
   end
   
   def accept
-    @proposal = Proposal.where(:div_id => current_user.divs, :id => params[:id]).first
+    @proposal = Proposal.where(:post_id => current_user.posts, :id => params[:id]).first
     if @proposal
       @proposal.status_type = :accept
       if @proposal.save
-        redirect_to new_div_url, :notice  => "Accept the proposal."
+        redirect_to new_post_url, :notice  => "Accept the proposal."
         # render :nothing => true, :status => :ok  #200
       else
         # render :nothing => true, :status => :internal_server_error #500
-        redirect_to new_div_url, :notice  => "Failed to accept proposal, try again."
+        redirect_to new_post_url, :notice  => "Failed to accept proposal, try again."
       end      
     else
         # render :nothing => true, :status => :not_found #404
-        redirect_to new_div_url, :notice  => "Failed to accept proposal, try again later."
+        redirect_to new_post_url, :notice  => "Failed to accept proposal, try again later."
     end
   end
   
   def reject
-    @proposal = Proposal.where(:div_id => current_user.divs, :id => params[:id]).first
+    @proposal = Proposal.where(:post_id => current_user.posts, :id => params[:id]).first
     if @proposal
       @proposal.status_type = :reject
       if @proposal.save
-        redirect_to new_div_url, :notice  => "Reject the proposal."
+        redirect_to new_post_url, :notice  => "Reject the proposal."
         # render :nothing => true, :status => :ok  #200
       else
-        redirect_to new_div_url, :notice  => "Failed to reject the proposal, try again."
+        redirect_to new_post_url, :notice  => "Failed to reject the proposal, try again."
         # render :nothing => true, :status => :internal_server_error #500
       end      
     else
-      redirect_to new_div_url, :notice  => "Failed to reject the proposal, try again later."
+      redirect_to new_post_url, :notice  => "Failed to reject the proposal, try again later."
         # render :nothing => true, :status => :not_found #404
     end    
   end
@@ -87,7 +87,7 @@ class ProposalsController < ApplicationController
   def update
     @proposal = current_user.proposals.find(params[:id])
     if @proposal.update_attributes(params[:proposal])
-      redirect_to new_div_url, :notice  => "Successfully updated proposal."
+      redirect_to new_post_url, :notice  => "Successfully updated proposal."
     else
       render :action => 'edit'
     end
@@ -96,6 +96,6 @@ class ProposalsController < ApplicationController
   def destroy
     @proposal = current_user.proposals.find(params[:id])
     @proposal.destroy
-    redirect_to new_div_url, :notice => "Successfully destroyed proposal."
+    redirect_to new_post_url, :notice => "Successfully destroyed proposal."
   end
 end
