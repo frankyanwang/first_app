ebda6f6c8ddbad106100f5169ab8fe8e67ceef34
Frank Wang <frankyanwang@gmail.com>
2011-10-02 22:46:28 -0700

    Remove hack to Store images into Rails Root tmp folder.

--- config.ru
+++ config.ru
@@ -2,8 +2,4 @@
 
 require ::File.expand_path('../config/environment',  __FILE__)
 
-# The basic idea is to save the temporary uploaded files in ./tmp/uploads/ and to tell Rack via Rack::Static to serve any
-# requests coming in for /uploads from ./tmp/uploads.
-# use Rack::Static, :urls => ['/uploads'], :root => 'tmp'
-
 run FirstApp::Application
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,5 @@
 CarrierWave.configure do |config|
-  # used for store images in Rails root folder tmp directories where Heroku is allowed to write files to.
-  # config.root = Rails.root.join('tmp')
-  # config.cache_dir = 'carrierwave'    
-
+  
   config.s3_access_key_id = ENV['s3_access_key_id']
   config.s3_secret_access_key = ENV['s3_secret_access_key']
   config.s3_bucket = ENV['s3_bucket']
