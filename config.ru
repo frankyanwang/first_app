7c030f5a0772cc5477060d37e43e59de8ae1bb7c
Frank Wang <frankyanwang@gmail.com>
2011-10-02 22:45:11 -0700

    Another way to enable Heroku store images wihtout using external storage Amazon S3 is to store images into Rails root tmp folder. Then we need to change RACK:Static to server images from there. But it is not going to be production quality service. Because it won't work with more than one Dyno, which has different tmp folders.This check in is to show how we disable this feature. Next check in will remote it completed.
    Ref: http://rickenharp.posterous.com/using-carrierwave-caching-on-heroku
    https://github.com/jnicklas/carrierwave/wiki/How-to%3A-Make-Carrierwave-work-on-Heroku

--- config.ru
+++ config.ru
@@ -1,4 +1,9 @@
 # This file is used by Rack-based servers to start the application.
 
 require ::File.expand_path('../config/environment',  __FILE__)
+
+# The basic idea is to save the temporary uploaded files in ./tmp/uploads/ and to tell Rack via Rack::Static to serve any
+# requests coming in for /uploads from ./tmp/uploads.
+# use Rack::Static, :urls => ['/uploads'], :root => 'tmp'
+
 run FirstApp::Application
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,7 @@
 CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrierwave'
+  # used for store images in Rails root folder tmp directories where Heroku is allowed to write files to.
+  # config.root = Rails.root.join('tmp')
+  # config.cache_dir = 'carrierwave'    
 
   config.s3_access_key_id = ENV['s3_access_key_id']
   config.s3_secret_access_key = ENV['s3_secret_access_key']
