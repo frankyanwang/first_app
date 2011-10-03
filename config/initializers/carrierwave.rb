a04a44badec5fd76db19fa531915010f1447b9ea
Frank Wang <frankyanwang@gmail.com>
2011-10-02 23:13:18 -0700

    Found out that still need to set carrierwave root folder to be in Rails root tmp folder. I guess when upload, it will temporarily store images there then push to Amazon S3. So enable this in Production environmen only. cache_dir carrierwave_cache folder will created as the same folder as the "uploads" folder.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,11 @@
 CarrierWave.configure do |config|
+
+  # used for store images in Rails root folder tmp directories where Heroku is allowed to write file
+  if ENV["RAILS_ENV"] == "production"
+    config.root = Rails.root.join('tmp')      
+  end
+  
+  config.cache_dir = 'carrierwave_cache'
   
   config.s3_access_key_id = ENV['s3_access_key_id']
   config.s3_secret_access_key = ENV['s3_secret_access_key']
