9e9d8cbebe875322a0373686f7b5624edbe0b017
Leon Qi <qi.leon@gmail.com>
2011-10-16 21:48:46 -0700

    Favorite feature. Added favorite in page feed_line and single item view for post and a nav page - Favorite.

---
+++ app/models/favorite.rb
@@ -0,0 +1,9 @@
+class Favorite < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :post
+  
+  attr_accessible :user_id, :post_id
+  
+  validates :post_id, :uniqueness => {:scope => :user_id, :message => "should only favorite once."}
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111016002915) do
+ActiveRecord::Schema.define(:version => 20111016183258) do
 
   create_table "comments", :force => true do |t|
     t.string   "description"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(:version => 20111016002915) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "favorites", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "favorites", ["post_id"], :name => "index_favorites_on_post_id"
+  add_index "favorites", ["user_id"], :name => "index_favorites_on_user_id"
 
   create_table "followships", :force => true do |t|
     t.integer  "user_id"
