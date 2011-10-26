9371f4f34ea7c84b291d53c798a42f2da6f659e4
Frank Wang <frankyanwang@gmail.com>
2011-10-25 22:53:02 -0700

    Use database session store instead of cookie based session store.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111024080238) do
+ActiveRecord::Schema.define(:version => 20111026054828) do
 
   create_table "comments", :force => true do |t|
     t.string   "description"
@@ -88,6 +88,16 @@ ActiveRecord::Schema.define(:version => 20111024080238) do
   end
 
   add_index "proposals", ["post_id", "trade_post_id"], :name => "index_proposal_on_post_id_trade_post_id", :unique => true
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
